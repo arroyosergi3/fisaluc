42c0ce480b4c61f85a0bd8e87a1748c966d8b774
Arroyosergi3 <sergioaj815@gmail.com>
2025-05-26 17:03:51 +0200

    some changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,29 +16,29 @@
     <div class="w-100 ">
         <div id="indicators-carousel" class="relative w-full" data-carousel="slide">
             <!-- Carousel wrapper -->
-            <div class="relative h-96 overflow-hidden rounded-lg md:h-[50vh] sm:h[25vh]">
+            <div class="relative h-[24rem] md:h-[42rem] overflow-hidden rounded-lg">
                 <!-- Item 1 -->
-                <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                <div class="hidden duration-700  ease-in-out object-cover absolute   " data-carousel-item="active">
                     <img src="{{ asset('car_img_1.jpg') }}"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 bottom-0 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 2 -->
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="{{ asset('car_img_2.jpg') }}"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <div class="hidden duration-700  ease-in-out object-cover absolute  bottom-0 " data-carousel-item>
+                    <img src="{{ asset('car_img_2_sin_recortar.jpg') }}"
+                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 bottom-0 left-1/2" alt="...">
                 </div>
                 <!-- Item 3 -->
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <div class="hidden duration-700  ease-in-out object-cover absolute  bottom-0 " data-carousel-item>
                     <img src="{{ asset('car_img_3.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 4 -->
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <div class="hidden duration-700  ease-in-out object-cover absolute  bottom-0 " data-carousel-item>
                     <img src="{{ asset('car_img_4.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 5 -->
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                <div class="hidden duration-700  ease-in-out object-cover absolute  bottom-0 " data-carousel-item>
                     <img src="{{ asset('car_img_5.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
